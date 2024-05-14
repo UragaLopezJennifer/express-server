90e8be109d8063e500c20792e08dcc469a9c4871
Jennifer Uraga Lopez <jenny@jennifers-mbp.mynetworksettings.com>
2024-05-14 15:53:51 -0400

    Run on port 3000 and return the message 'Server is operational' if a client makes a get request to the route /test

---
+++ index.js
@@ -0,0 +1,24 @@
+// import express into our file
+const express = require('express')
+
+// create a new express application
+const app = express()
+
+// define the port our server will run on
+const port = 3000
+
+// define our server routes
+
+app.get('/test', ( req, res )=>{
+    // do something when the server processes this request
+
+    // send back a response to the client
+    res.send("Server is operational")
+
+})
+
+
+// run our server to listen at the port we defined
+app.listen(port, ()=>{
+    console.log('Server is running on port ${port}')
+})
