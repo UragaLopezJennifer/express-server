d6de2cd5635cf75db5ee04930f6f072e475ea60f
Jennifer Uraga Lopez <jenny@jennifers-mbp.mynetworksettings.com>
2024-05-14 16:56:57 -0400

    Return an image as a static asset

--- index.js
+++ index.js
@@ -7,15 +7,26 @@ const app = express()
 // define the port our server will run on
 const port = 3000
 
+
+// allow statoc assets in public folder
+app.use(express.static('public')
+
+
+)
 // define our server routes
+
+app.get('/', ( req, res )=>{
+    res.send("Server running functionally!")
+})
 
 app.get('/test', ( req, res )=>{
     // do something when the server processes this request
 
     // send back a response to the client
     res.send("Server is operational")
-
 })
+
+
 
 
 // run our server to listen at the port we defined
